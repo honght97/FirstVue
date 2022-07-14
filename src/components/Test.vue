4e24ce816c365849bc0f233088c6a29ca2a6581a
honght97 <hong.ht150297@gmail.com>
2022-07-15 01:40:52 +0700

    learn data methods and v-if

---
+++ src/components/Test.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        Hello from {{lastName}} and {{isHello ? `${firsName}  ${lastName}` : ''}}
+    </div>
+    <button @click="onChangeName">Change Name</button>
+    <div class="modal" v-if="isShowModal">
+        <p>Modal content</p>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                firsName: "Hoang",
+                lastName: "Hong",
+                isHello: true,
+                isShowModal: false,
+            }
+        },
+        methods: {
+            onChangeName() {
+             this.firsName = "Thao";
+             this.isShowModal = !this.isShowModal;
+            }
+        }
+    }
+</script>
